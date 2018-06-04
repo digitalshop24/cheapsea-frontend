46ef4c3111395bb1441b72720b1cc97b4ffa2d01
iteam4u <iteam4u@ya.ru>
2018-06-05 01:24:29 +0300

    Сборка, стилизация

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import './core/hooks';
 import Vue from 'vue';
 import axios from 'axios';
 import VueMapp from 'vue-mapp';
+import './core/hooks';
+import './css/@app.css';
 import VueAgile from 'vue-agile';
 import VueCarousel from 'vue-carousel';
 import App from './App.vue';
@@ -9,7 +10,6 @@ import store from './store';
 import router from './router';
 import filters from './helpers/filters';
 import controls from './components/controls';
-import './css/@app.css';
 
 Vue.config.productionTip = false;
 axios.defaults.baseURL = 'https://cheapsea.net/api/v1';
@@ -18,7 +18,7 @@ Vue.use(controls);
 Vue.use(filters);
 Vue.use(VueMapp);
 Vue.use(VueCarousel);
-Vue.use(VueAgile);
+Vue.use(VueAgile)
 
 new App({
     el: '#app',
